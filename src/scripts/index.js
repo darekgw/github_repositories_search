9ff76c71bb3dcafcb581b37deaa55f7bc493c073
Dariusz Gwozdz <darekgw@interia.pl>
2020-05-30 20:52:04 +0200

    intial commit

---
+++ src/scripts/index.js
@@ -0,0 +1,15 @@
+import '../stylesheets/style.scss';
+import ReposSearch from '../ReposSearch';
+
+document.addEventListener('DOMContentLoaded', function () {
+	const reposSearch = new ReposSearch();
+	const searchButton = document.querySelector('.search__button');
+	const input = document.querySelector(".search__input");
+
+	searchButton.addEventListener('click', () => {
+		reposSearch.getDataFromInput()
+	});
+	input.addEventListener('keyup', (e) => {
+		if(e.keyCode === 13) reposSearch.getDataFromInput();
+	});
+});
